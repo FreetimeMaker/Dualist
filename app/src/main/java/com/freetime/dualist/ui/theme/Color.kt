6b28726efa333dc57ac0aea955af99292d932302
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2026-05-23 21:57:30 +0200

    Initial commit

---
+++ app/src/main/java/com/freetime/dualist/ui/theme/Color.kt
@@ -0,0 +1,39 @@
+package com.freetime.dualist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Vibrant, Energetic Color Palette (M3)
+// Light Scheme
+val LightPrimary = Color(0xFFD0BCFF)
+val LightOnPrimary = Color(0xFF381E72)
+val LightPrimaryContainer = Color(0xFF4F378B)
+val LightOnPrimaryContainer = Color(0xFFEADDFF)
+val LightSecondary = Color(0xFFCCC2DC)
+val LightOnSecondary = Color(0xFF332D41)
+val LightSecondaryContainer = Color(0xFF4A4458)
+val LightOnSecondaryContainer = Color(0xFFE8DEF8)
+val LightTertiary = Color(0xFFEFB8C8)
+val LightOnTertiary = Color(0xFF492532)
+val LightTertiaryContainer = Color(0xFF633B48)
+val LightOnTertiaryContainer = Color(0xFFFFD8E4)
+
+// Dark Scheme
+val DarkPrimary = Color(0xFF6750A4)
+val DarkOnPrimary = Color(0xFFFFFFFF)
+val DarkPrimaryContainer = Color(0xFFEADDFF)
+val DarkOnPrimaryContainer = Color(0xFF21005D)
+val DarkSecondary = Color(0xFF625B71)
+val DarkOnSecondary = Color(0xFFFFFFFF)
+val DarkSecondaryContainer = Color(0xFFE8DEF8)
+val DarkOnSecondaryContainer = Color(0xFF1D192B)
+val DarkTertiary = Color(0xFF7D5260)
+val DarkOnTertiary = Color(0xFFFFFFFF)
+val DarkTertiaryContainer = Color(0xFFFFD8E4)
+val DarkOnTertiaryContainer = Color(0xFF31111D)
+
+// Additional vibrant colors for task states
+val VibrantYellow = Color(0xFFFFD600)
+val VibrantGreen = Color(0xFF00C853)
+val VibrantRed = Color(0xFFFF1744)
+val VibrantOrange = Color(0xFFFF6D00)
+val VibrantPink = Color(0xFFFF4081)
